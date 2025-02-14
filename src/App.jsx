2d350ce9c377a6bed61cc9dce0ee195b29fd64cf
Emmanuel oshakpemeh <149076550+imacul@users.noreply.github.com>
2025-02-14 09:14:09 +0000

    add fully functional  trending movies  section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/Search';
 import { Spinner } from "flowbite-react";
 import MovieCard from './components/MovieCard';
 import { useDebounce } from "react-use";
+import { getTrendingMovies, updateSearchCount } from "./appwrite";
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_ACESS_TOKEN;
@@ -21,6 +22,19 @@ const App = () => {
     const [movieList, setMovieList] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [debounceSearchTerm, setDebounceSearchTerm] = useState('');
+    const [ trendingMovies, setTrendingMovies] = useState([]);
+
+     
+    const loadTrendingMovies = async () => {
+        try {
+            const movies  = await getTrendingMovies();
+
+            setTrendingMovies(movies);
+        } catch (error) {
+            console.log(`Error fetching movies ${error}`);
+        }
+    }
+    
 
     const fetchMovies = async (query = "") => {
         setErrorMessage('');
@@ -41,11 +55,14 @@ const App = () => {
                             setMovieList([]);
                                   return;
                                   }
-            
-            
+          setMovieList(data.results);
+          
 
-            setMovieList(data.results);
-
+          
+     if(query && data.results.length > 0 ) {
+        await updateSearchCount(query, data.results[0]);
+     }
+          
         } catch (error) {
             console.error(`Error fetching movies: ${error}`);
             setErrorMessage('Error fetching movies, please try again later.');
@@ -61,7 +78,12 @@ const App = () => {
             await fetchMovies(searchTerm);
         };
         getMovies();
-    }, [searchTerm]);
+    }, [debounceSearchTerm]);
+
+
+    useEffect(() => {
+        loadTrendingMovies();
+    }, []);
 
     return (
         <main>
@@ -74,8 +96,24 @@ const App = () => {
                         </h1>
                         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
                     </header>
+                    
+                    {trendingMovies.length > 0 && (
+                        <section className="trending">
+                          <h2>Trending Movies</h2>
+              
+                          <ul>
+                            {trendingMovies.map((movie, index) => (
+                              <li key={movie.$id}>
+                                <p>{index + 1}</p>
+                                <img src={movie.poster_url} alt={movie.title} />
+                              </li>
+                            ))}
+                          </ul>
+                        </section>
+                      )}
+              
                     <section className="all-movies">
-                        <h1 className='mt-[20px]'>All Movies</h1>
+                        <h1>All Movies</h1>
                         {isLoading ? (
                         <div className='text-center justify-center items-center'>
                             <Spinner />
